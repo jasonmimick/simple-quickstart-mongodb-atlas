47a9e6cd7d804557aeea2cb8a961b7fdf056f57e
Jason Mimick <jmimick@gmail.com>
2020-10-15 10:16:45 -0400

    actions for lambda deploy and launch quickstart

--- scripts/deploy.MongoDBCloudResourceManager_lambda.sh
+++ scripts/deploy.MongoDBCloudResourceManager_lambda.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
 
+
+
+echo "which aws=$(which aws)"
+aws help
+
 # Deploys a CloudFormation stack running the MongoDB Cloud Resource Manager
-set -ex
 STACK_NAME="${1:-mongodb-cloud-resource-manager}"
 echo "STACK_NAME=${STACK_NAME}"
 REGION="${2:-$(aws configure get region)}"
 echo "REGION=${REGION}"
 TARGET="$(git rev-parse --show-toplevel)/templates/mongodbatlas-resource-manager.template.yaml"
 echo "TARGET=${TARGET}"
-echo "input;;; $*"
-echo "bounce - will delete ${STACK_NAME} and create"
-aws cloudformation delete-stack --stack-name "${STACK_NAME}" \
-    --region "${REGION}"
-echo "deleted stack, nap time"
-sleep 10
-echo "up & adam"
+EXISTS=$(aws cloudformation describe-stacks --output json --region ${REGION} --stack-name ${STACK_NAME} 2>&1)
+echo "EXISTS=${EXISTS}"
+echo ""
+if [[ -z "${EXISTS}" ]]
+then
+    echo "${STACK_NAME} not found"
+else
+    aws cloudformation delete-stack --output json --stack-name "${STACK_NAME}" --region "${REGION}"
+    echo "deleted stack, nap time"
+    sleep 10
+    echo "up & adam"
+fi
+
 aws cloudformation create-stack --stack-name "${STACK_NAME}" \
-    --region "${REGION}" \
+    --region "${REGION}" --output json \
     --template-body "file://${TARGET}" \
     --capabilities CAPABILITY_IAM
 
