15e62438a1e902c6c069903cc21fcd40c64ff114
Jason Mimick <jmimick@gmail.com>
2020-10-28 16:01:59 -0400

    better delete wait, peering support

--- scripts/launch-new-quickstart-lambda-deployed-apikey.sh
+++ scripts/launch-new-quickstart-lambda-deployed-apikey.sh
@@ -3,4 +3,5 @@ STACK_NAME="${1:-aws-quickstart}"
 aws cloudformation create-stack \
   --capabilities CAPABILITY_IAM --disable-rollback \
   --template-body file://templates/quickstart-mongodb-atlas.template.yaml \
+
   --stack-name "${STACK_NAME}"
